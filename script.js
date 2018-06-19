3ae30b97c69b387c115a10d26007d2f66ae22482
loanpasionek <l.pasionek@gmail.com>
2018-06-19 15:25:38 +0200

    Ajout de la nav changeant de couleur selon la position dans la page (jQuery)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 M.AutoInit();
 
-
 /* ------------  Smooth scrolling ------------ */
 
 $('a[href^="#"]').on('click', function(event) {
@@ -8,7 +7,10 @@ $('a[href^="#"]').on('click', function(event) {
     if( target.length ) {
         event.preventDefault();
 
+
+        /* !!!!!!!!!!!!!!!! Calculer hauteur nav pour scrolling exact !!!!!!!!!!!!!!!!! */
         var scroll = target.offset().top - 90;
+        
         $('html, body').stop().animate({
             scrollTop: scroll
         }, 1000);
@@ -30,21 +32,36 @@ $(document).ready(function(){
 $(function(){
 
     var $nav = $('nav');
-    var $liNav = $('linav');
+    /* var $burger = $('buttonBurger'); */
+    var $headerHeight = $("header").height();
+    var $aboutHeight = $("#about").height() + $("header").height();
+
 $(window).scroll(function()
     {
-            var scroll = $(window).scrollTop();
-            if(scroll > 435)
+        var scroll = $(window).scrollTop();
+        if(scroll > $headerHeight)
         {
         $nav.addClass('navChange');
-        $liNav.addClass('color4');
         }
         else
         {
-            if(scroll <= 435 ){
+            if(scroll <= $headerHeight ){
 
             
             $nav.removeClass('navChange');
+            }
+        }
+        if(scroll >  $aboutHeight)
+        {
+        $nav.addClass('navChange2');
+        /* $burger.addClass('color4'); */
+        }
+        else
+        {
+            if(scroll <=  $aboutHeight ){
+
+            
+            $nav.removeClass('navChange2');
             }
         }
     })
