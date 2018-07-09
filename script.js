e54fea9f3a75acc6704d1f821320e0ac43ecf05c
loanpasionek <l.pasionek@gmail.com>
2018-07-09 10:20:46 +0200

    correction sur le changement de couleur de la nav (JS)

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ M.AutoInit();
 
 $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
-    if( target.length ) {
+    var $nav = $('nav').height();
+    if(target.length) {
         event.preventDefault();
 
 
-        /* !!!!!!!!!!!!!!!! Calculer hauteur nav pour scrolling exact !!!!!!!!!!!!!!!!! */
-        var scroll = target.offset().top - 90;
+        var scroll = target.offset().top - $nav;
         
         $('html, body').stop().animate({
             scrollTop: scroll
@@ -31,39 +31,38 @@ $(document).ready(function(){
 
 $(function(){
 
-    var $nav = $('nav');
-    /* var $burger = $('buttonBurger'); */
+    var nav = $('nav');
     var $headerHeight = $("header").height();
     var $aboutHeight = $("#about").height() + $("header").height();
+    var aLiens = $('#nav-mobile a');
 
 $(window).scroll(function()
     {
         var scroll = $(window).scrollTop();
-        if(scroll > $headerHeight)
-        {
-        $nav.addClass('navChange');
-        }
-        else
-        {
+        
+        console.log(scroll + ' / ' + $headerHeight + ' / ' + $aboutHeight);
+
+        if(scroll > $headerHeight){
+            nav.removeClass('nocolor').addClass('navChange');
+        } else {
             if(scroll <= $headerHeight ){
-
-            
-            $nav.removeClass('navChange');
+                nav.removeClass('navChange').addClass('nocolor');
             }
         }
-        if(scroll >  $aboutHeight)
-        {
-        $nav.addClass('navChange2');
-        /* $burger.addClass('color4'); */
-        }
-        else
-        {
+
+        if(scroll >  $aboutHeight){
+            nav.removeClass('navChange').addClass('navChange2');
+            aLiens.each(function(){
+                $(this).addClass('color4').removeClass('divider-color');
+            });
+        } else {
             if(scroll <=  $aboutHeight ){
-
-            
-            $nav.removeClass('navChange2');
+                nav.removeClass('navChange2');
+                aLiens.each(function(){
+                    $(this).addClass('divider-color').removeClass('color4');
+                });
             }
         }
-    })
+    });
     
     });
